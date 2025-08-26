3d5d8e2d57ac1e22052a8454054f511fcd0e32c7
HANSEOHYUN <000312@daum.net>
2025-08-27 00:42:38 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/extension_guard/ExtensionGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.extension_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/extension_guard/ExtensionGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extension_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
